1475a8c3e3e2ea3e2a8c3d20327f17adf4dc1f67
Woobin Jang <wbnjang@gmail.com>
2026-01-07 16:16:04 -0800

    resolve

--- leetcode/hashing/containsDuplicate.py
+++ leetcode/hashing/containsDuplicate.py
@@ -18,3 +18,12 @@ def containsDuplicateFasterHybrid(nums):
             return True
         seen.add(num)
     return False
+
+def containsDuplicateResolve(nums):
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    
+    return False

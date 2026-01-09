1c527ff8c20e718d9b19dca5654e90739f0854fd
Woobin Jang <wbnjang@gmail.com>
2026-01-09 15:16:23 -0800

    palindrome number

--- leetcode/strings/palindromeNumber.py
+++ leetcode/strings/palindromeNumber.py
@@ -17,5 +17,21 @@ def isPalindrome(x):
     return x == t
 
 
-print(isPalindrome(12321))
+def isPalindromeAnswer(x):
+    if x < 0:
+        return False
+    
+    reversedHalf = 0
+    while(x > reversedHalf):
+        #Take what we have, move it over so the 1's digit is empty, then we put in the last digit of x
+        reversedHalf = reversedHalf * 10 + x // 10
+        #remove the digit we moved over to reversed half
+        x = x//10
+    
+    #or is there for odd lengths, becaus the middle digit will be in the reversed half, we drop it by doing //10 and then compare
+    return x == reversedHalf or x == reversedHalf//10
+    
+
+    
+
 

e181435492a9e39cc3d7b4e583f3c53b92a523c8
Woobin Jang <wbnjang@gmail.com>
2026-03-10 23:14:26 -0700

    twoSum

--- arrays & hashing/twoSum.py
+++ arrays & hashing/twoSum.py
@@ -2,41 +2,17 @@
 #Space Complexity : O(n)
 
 def twoSum(nums, target):
-    ans = []
-    hashT = {}
-    for x in range(len(nums)):
-        hashT[x] = nums[x]
-    
-    i = 0
-    for x in nums:
-        if (target-x) in hashT.values() and (target-x) in nums[i+1:]:
-            ans.append(i)
-            ans.append(nums.index(target-x, i+1))
-            return ans
-        i+=1
-
-
-def twoSumAnswer(nums, target):
-    #we make a dictionary
-    hash_map = {} 
-    
-    for i, num in enumerate(nums):
-        complement = target - num
-        
-        if complement in hash_map:
-            return [hash_map[complement], i]
-        
-        hash_map[num] = i
-            
-
-
-def twoSumResolve(nums, target):
     seen = {}
-    for i, value in enumerate(nums):
-        complement = target - value
-        if complement in seen:
-            return [i, seen[complement]]
-        seen[value] = i
+    for index in range(0, len(nums)):
+        num = nums[index]
+        if target - num in seen:
+            return [seen[target-num], index]
+        seen[num] = index
 
+
+nums = [2,7,11,15]
+target = 9
+        
+print(twoSum(nums, target))
 
 

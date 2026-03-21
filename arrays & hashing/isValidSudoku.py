ad96178cf2ef4d7ef98fae2cb8c26440c9451caf
Woobin Jang <wbnjang@gmail.com>
2026-03-20 20:09:15 -0700

    valid sudoku

---
+++ arrays & hashing/isValidSudoku.py
@@ -0,0 +1,86 @@
+from collections import defaultdict
+
+def isValidSudoku(board):
+    rowSeen = defaultdict(set)
+    colSeen = defaultdict(set)
+    squareSeen = defaultdict(set)
+    squareRow = 0
+    squareCol = 1
+
+
+    for rowNum in range(0,9):
+        if rowNum > 2:
+            squareRow = 3
+        if rowNum > 5:
+            squareRow = 6
+
+        row = board[rowNum]
+        for colNum in range(0, 9):
+            if colNum <= 8:
+                squareCol = 3
+            if colNum <= 5:
+                squareCol = 2
+            if colNum <=2:
+                squareCol = 1
+            squareNum = squareRow + squareCol
+
+            numSeen = row[colNum]
+            if numSeen == ".":
+                continue
+            if numSeen in rowSeen[rowNum]:
+                return False
+            rowSeen[rowNum].add(numSeen)
+
+            if numSeen in colSeen[colNum]:
+                return False
+            colSeen[colNum].add(numSeen)
+
+            if numSeen in squareSeen[squareNum]:
+                return False
+            squareSeen[squareNum].add(numSeen)
+        
+    return True
+
+
+def isValidSudokuAns(board):
+    rowSeen = defaultdict(set)
+    colSeen = defaultdict(set)
+    squareSeen = defaultdict(set)
+
+    for rowNum in range(9):
+        print(f"rownum is {rowNum}")
+        for colNum in range(9):
+            squareNum = (rowNum // 3) * 3 + (colNum // 3)
+            numSeen = board[rowNum][colNum]
+
+            if numSeen == ".":
+                continue
+
+            if numSeen in rowSeen[rowNum]:
+                return False
+            rowSeen[rowNum].add(numSeen)
+
+            if numSeen in colSeen[colNum]:
+                return False
+            colSeen[colNum].add(numSeen)
+
+            if numSeen in squareSeen[squareNum]:
+                return False
+            squareSeen[squareNum].add(numSeen)
+        
+    return True
+
+            
+
+
+board = [["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+
+print(isValidSudokuAns(board))

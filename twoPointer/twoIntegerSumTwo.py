af91991531e1a4eeccca7cfe14f4a9bc7e160329
Woobin Jang <wbnjang@gmail.com>
2026-03-30 16:16:56 -0700

    two sum 2

---
+++ twoPointer/twoIntegerSumTwo.py
@@ -0,0 +1,37 @@
+def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    i = 0
+    while i < len(numbers) -1:
+
+        if i < len(numbers) -1 and target - numbers[i] > numbers[-1]:
+            i +=1
+
+        j = i+1
+
+        while j < len(numbers):
+            if numbers[i] + numbers[j] == target:
+                return [i+1, j+1]
+            else:
+                j +=1
+
+        i +=1
+            
+
+def twoSumAns(numbers, target):
+    i = 0
+    j = len(numbers) -1
+
+    while i < j:
+        res = numbers[i] + numbers[j]
+        if res > target:
+            j -=1
+        else:
+            if res == target:
+                return [i+1, j+1]
+            i +=1
+
+
+
+        
+        
+
+

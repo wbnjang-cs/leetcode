977a56a944e63f42c497c42ac239fd4c407d0476
Woobin Jang <wbnjang@gmail.com>
2026-05-02 01:45:53 -0700

    rotated binary search

--- binarySearch/rotatedArraySearch.py
+++ binarySearch/rotatedArraySearch.py
@@ -8,7 +8,12 @@ def search(nums, target):
             return m
         
         #nums[i] is smallest, nums[m] is biggest in left side
+        #important*********************
+        #has to be <=, NOT <.
+        #If i == m, then we will use right side sorted logic when left side is sorted (1 element is always sorted)
+        #Basically using < when i == m assumes that a single element is not sorted and goes to wrong logic
         if nums[i] <= nums[m]:
+        #important*********************
             if nums[m] < target or nums[i] > target:
                 i = m + 1
             else:
